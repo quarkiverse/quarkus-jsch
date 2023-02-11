eadd8f0e03267220e8f637836471c02348a1f542
Peter Palaga <ppalaga@redhat.com>
2023-02-11 17:14:16 +0100

    Upgrade to Quarkus 3 (#78)
    Fix https://github.com/quarkiverse/quarkus-jsch/issues/77

--- integration-tests/src/main/java/io/quarkus/it/jsch/JSchResource.java
+++ integration-tests/src/main/java/io/quarkus/it/jsch/JSchResource.java
@@ -1,9 +1,9 @@
 package io.quarkus.it.jsch;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
